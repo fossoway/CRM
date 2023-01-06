d78f40bb9b8851e512b75fe4b08937495bdca37f
Elena Bukina <selenaastu@gmail.com>
2023-01-06 22:19:36 +0400

    add script.js, update index.html, form.css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+{
+  const modWindow = document.querySelector('.overlay');
+  modWindow.classList.toggle('overlay-flex');
+  const headModWindow = document.querySelector('.form__title');
+  const buttonIdModWindow = document.querySelector('.form__id');
+  const idModWindow = document.querySelector('.form__numb');
+  const formModWindow = document.querySelector('.form__form');
+  const checkbox = document.querySelector('.checkbox__input');
+  const discount = document.querySelector('.form__input-discount');
+  const totalCost = document.querySelector('.form__cost');
+}
